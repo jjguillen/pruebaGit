71179d06dc0f2d073d14403728e766a66a9ee9f6
jjguillen <jjavierguillen@gmail.com>
2023-10-25 18:26:40 +0200

    ejemplogit

--- EjemploGit.java
+++ EjemploGit.java
@@ -1,5 +1,13 @@
 public class EjemploGit {
     public static void main(String[] args) {
         System.out.println("Hola Git");
+        int numero=0;
+        try {
+            numero = Integer.parseInt("3");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(numero);
     }
 }
